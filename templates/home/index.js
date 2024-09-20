33943156067516965ee61a8a9dd5de8e94de9ede
Jay Shah <jay@jays-air.home>
2024-09-20 17:57:06 -0400

    Created Home Page using HTML, CSS, JS

---
+++ templates/home/index.js
@@ -0,0 +1,7 @@
+document.querySelector(".login-btn").addEventListener("click", () => {
+    alert("Login button clicked!");
+});
+
+document.querySelector(".cart-btn").addEventListener("click", () => {
+    alert("Add to Cart clicked!");
+});
